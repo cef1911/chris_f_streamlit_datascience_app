f20cb179978a3c484b8e6dad79d6aef776584bda
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-19 05:50:50 -0500

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -4,7 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import numpy as np
-
+import plotly.figure_factory as ff
 
 
 """
@@ -39,6 +39,22 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 # st.bar_chart(chart_data)
 
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
