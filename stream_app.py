f925da6f9ff3212618bc5df996e6f1f14a2c12e6
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-23 18:32:30 -0500

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -91,3 +91,9 @@ st.write("Streamlit Play:) creating dataframes and plotly plots")
 
 
 
+from datetime import time
+
+appointment = st.slider(
+    "Schedule your appointment:",
+    value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
