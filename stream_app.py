f297c0ba062e450ed282613c923cf50512ff9c86
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-27 12:09:18 -0500

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -150,13 +150,13 @@ st.text_input("Your name", key="name")
 st.session_state.name
 
 
-# df = pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-#     })
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
 
-# option = st.selectbox(
-#     'Which number do you like best?',
-#      df['first column'])
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
 
-# 'You selected: ', option
+'You selected: ', option
