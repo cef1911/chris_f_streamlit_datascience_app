466c80a5269bb48e6d54d71711f153e1df5ac596
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-27 12:06:14 -0500

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -135,3 +135,28 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+     
+ st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
+
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
