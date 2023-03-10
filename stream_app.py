d3d92dd9fa50c918004c787ffbb7c07b85818eaa
Christopher Enam Franklin <cef1911@hotmail.com>
2023-03-10 10:53:43 -0500

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -11,7 +11,7 @@ import time
 
 
 """
-# Welcome to Chris Franklin's StreamLit Web App for Deploying DataScience Projects!
+# Welcome to Chris Franklin's StreamLit Web App for Deploying DataScience Projects Khori have fun!
 This is inspired from the StreamLit.io site.
 See the website for more info https://streamlit.io/
 
